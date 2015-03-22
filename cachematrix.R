cc7aa2e6d8599baea1ee95841cc3a50a3220d562
Matt K Williams <speedymwill@gmail.com>
2015-03-22 13:47:00 +0000

    Completed week 2 assignment for peer evaluation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions to calculate and cache the inverse of a matrix "x"
 
-## Write a short comment describing this function
 
+## Create a list of four functions to calculate, store and retrieve the inverse of matrix "x"
 makeCacheMatrix <- function(x = matrix()) {
 
+## clear cache
+      mat <- NULL
+
+## function to pass the input matrix to "x", and clear cache, as the inverse of this new matrix has not been calculated
+      set <- function(y) {
+            x <<- y
+            mat <<- NULL
+      }
+
+## function to return the input matrix "x"
+      get <- function() x
+
+## function to pass the received matrix ("matrix") to the cache (for cachesolve, this will be the inverse matrix of x)
+      setmatrix <- function(matrix) mat <<- matrix
+
+## function to return the cached inverse matrix
+      getmatrix <- function() mat
+
+## return the created functions in a list
+      list(set = set, get = get,
+           setmatrix = setmatrix,
+           getmatrix = getmatrix)
+  
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of a matrix, retrieveing it from cache if available
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+
+      mat <- x$getmatrix()
+      
+## Check if a cached value is available, and if there is, return it
+      
+      if(!is.null(mat)) {
+            message("getting cached data")
+            return(mat)
+      }
+      
+## If no cached value, calculate inverse and store in cache
+
+      data <- x$get()
+      mat <- solve(data, ...)
+      x$setmatrix(mat)
+      mat
+
 }
